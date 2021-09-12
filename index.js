8cc5e6ebf16c9cad768e8ecc38623ec677d58875
Thanh Khiet <cs.thanhkhiet@gmail.com>
2021-09-12 11:20:09 +0700

    add mongo depend app

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 const express = require("express");
 const mongoose = require('mongoose');
-const app = express();
+const { MONGO_USER, MONGO_PASSWORD, MONGO_IP, MONGO_PORT } = require("./config/config");
 
-// mongodb://username:password@ipaddress:port (ipaddress of database)
+const app = express();
 // we can use name of service instead of ipaddress (DNS helps you resolve host name --> ip)
-mongoose.connect("mongodb://thanhkhiet:thanhkhiet@mongoDb:27017/?authSource=admin")
-//
+const mongoUrl = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`;
+
+mongoose.connect(mongoUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
     .then(() => console.log("Connected to DB successfully"))
     .catch((e) => console.log(e))
 
 const port = process.env.PORT || 2000;
 
 app.get("/", (req, res) => {
-    res.send("<h2>hello thanhkhiet</h2>")
+    res.send("<h2>hello mapden</h2>")
 });
 
 app.listen(port, () => {

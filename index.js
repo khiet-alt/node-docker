9db203e8ad85a009aa909de96234f32432531bff
Thanh Khiet <cs.thanhkhiet@gmail.com>
2021-09-12 10:13:54 +0700

    connect to container mongo

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require("express");
-
+const mongoose = require('mongoose');
 const app = express();
+
+// mongodb://username:password@ipaddress:port (ipaddress of database)
+// we can use name of service instead of ipaddress (DNS helps you resolve host name --> ip)
+mongoose.connect("mongodb://thanhkhiet:thanhkhiet@mongoDb:27017/?authSource=admin")
+//
+    .then(() => console.log("Connected to DB successfully"))
+    .catch((e) => console.log(e))
 
 const port = process.env.PORT || 2000;
 
